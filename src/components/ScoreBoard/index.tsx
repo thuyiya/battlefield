1152b19481df918e6f380fdc536524a9c3be2f47
thuyiya <thusitha.4t@gmail.com>
2022-12-23 08:02:40 +0530

    score board added

--- src/components/ScoreBoard/index.tsx
+++ src/components/ScoreBoard/index.tsx
@@ -1,5 +1,19 @@
+import ScoreBoardItems from '../ScoreBoardItems'
+import useApp from '../../hooks/useApp'
+
 const ScoreBoard = () => {
-  return <div>ScoreBoard</div>
+  const appContext = useApp()
+
+  return (
+    <div className='flex_center_row t-me-20'>
+      <ScoreBoardItems
+        bgColor='#f39c12'
+        player={appContext?.user?.name}
+        score={appContext?.user?.shoot}
+      />
+      <ScoreBoardItems bgColor='#1abc9c' />
+    </div>
+  )
 }
 
 export default ScoreBoard
---
+++ src/components/ScoreBoardItems/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react'
+
+type PropsScoreBoardItems = {
+  bgColor?: string
+  score?: number
+  player?: string
+}
+
+const ScoreBoardItems: FC<PropsScoreBoardItems> = ({ bgColor, score, player }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: bgColor || 'gray',
+      }}
+    >
+      <h1>{score || '00'}</h1>
+      <div className='line'/>
+      <h3 style={{ display: 'inline' }}>{player || 'player _'}</h3>
+    </div>
+  )
+}
+
+export default ScoreBoardItems
